e0f363ea9e456dc6c48fc40555af8ccbceee2254
valntyn <romanenko.valentyn1@gmail.com>
2023-05-09 10:44:10 +0300

    added ui

--- src/components/Header/NavigationBar/index.tsx
+++ src/components/Header/NavigationBar/index.tsx
@@ -1,16 +1,38 @@
+import classNames from 'classnames';
+import { useState } from 'react';
+
 import { ReactComponent as Arrow } from '@assets/svg/arrow-down.svg';
 
-import './NavigationBar.scss';
+import { Dropdown } from '../Dropdown';
 import { SearchBar } from '../SearchBar';
 
+import './NavigationBar.scss';
+
 export const NavigationBar = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = () => {
+    setIsHovered(!isHovered);
+  };
+
   return (
     <div className="nav-bar">
-      <div className="nav-bar__dropdown">
+      <div
+        className="nav-bar__dropdown"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+      >
         <span className="nav-bar__text">All categories</span>
         <div className="nav-bar__svg-box">
-          <Arrow className="nav-bar__svg" />
+          <Arrow
+            className={classNames('nav-bar__svg', {
+              'nav-bar__svg--active': isHovered,
+            })}
+          />
         </div>
+        {isHovered && (
+          <Dropdown />
+        )}
       </div>
       <div className="nav-bar__line" />
       <SearchBar />
