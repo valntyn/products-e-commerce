c81388e949fb5a10aa6b740747fe63b9df9d42c8
valntyn <romanenko.valentyn1@gmail.com>
2023-05-13 16:55:08 +0300

    hide search-bar on homepage

--- src/components/Header/MainBlock/index.tsx
+++ src/components/Header/MainBlock/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '@assets/svg/logo.svg';
 
@@ -8,12 +8,15 @@ import { SearchBar } from '../SearchBar';
 import './MainBlock.scss';
 
 export const MainBlock = () => {
+  const location = useLocation();
+  const isProductsPage = location.pathname === '/products';
+
   return (
     <div className="header-main">
       <Link to="/" className="header-main__link">
         <Logo className="header-main__logo" />
       </Link>
-      <SearchBar />
+      {isProductsPage && <SearchBar />}
       <Icons />
     </div>
   );
