c1c28a4fd6cf1fb27d776f5699c2cbec76e552ef
valntyn <romanenko.valentyn1@gmail.com>
2023-05-25 18:02:26 +0300

    changed max width

--- src/constants/carousel.ts
+++ src/constants/carousel.ts
@@ -2,4 +2,4 @@ export const CARD_WIDTH = 250;
 export const CARD_GAP = 32;
 export const SINLGE_SKIP = 1;
 export const DEFAULT_SKIP = 4;
-export const MAX_WIDTH = 1240;
+export const MAX_WIDTH = 1480;
