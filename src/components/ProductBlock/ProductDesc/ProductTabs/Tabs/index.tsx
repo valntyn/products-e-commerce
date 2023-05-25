b6e6d0e5e3c424874168ab83f297fe4b97a24b80
valntyn <romanenko.valentyn1@gmail.com>
2023-05-25 13:56:35 +0300

    fixed

--- src/components/ProductBlock/ProductDesc/ProductTabs/Tabs/index.tsx
+++ src/components/ProductBlock/ProductDesc/ProductTabs/Tabs/index.tsx
@@ -22,57 +22,52 @@ export const Tabs: React.FC<PropTypes> = ({
 
   const tabRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const recalculatePosition = () => {
     const selectedTab = tabRef.current?.querySelector(
       '.tabs__tab--selected',
     ) as HTMLElement;
 
-    const recalculatePosition = () => {
-      if (selectedTab) {
-        const { width, left } = selectedTab.getBoundingClientRect();
-        const containerLeft = tabRef.current?.getBoundingClientRect().left || 0;
+    if (selectedTab) {
+      const { width, left } = selectedTab.getBoundingClientRect();
+      const containerLeft = tabRef.current?.getBoundingClientRect().left || 0;
 
-        setAnimateStyle({
-          width,
-          transform: `translateX(${left - containerLeft}px)`,
-        });
-      }
-    };
+      setAnimateStyle({
+        width,
+        transform: `translateX(${left - containerLeft}px)`,
+      });
+    }
+  };
 
+  useEffect(() => {
     recalculatePosition();
 
-    const handleResize = () => {
-      recalculatePosition();
-    };
-
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', recalculatePosition);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('resize', recalculatePosition);
     };
   }, [selectedId]);
 
   return (
     <div className={classNames('tabs', className)} ref={tabRef}>
-      {tabs
-        && tabs.map((tab) => (
-          <div
-            key={tab.id}
-            className={classNames('tabs__tab', {
-              'tabs__tab--selected': tab.id === selectedId,
+      {tabs?.map((tab) => (
+        <div
+          key={tab.id}
+          className={classNames('tabs__tab', {
+            'tabs__tab--selected': tab.id === selectedId,
+          })}
+          onClick={() => onClick && onClick(tab.id)}
+        >
+          <p
+            className={classNames('tabs__label', {
+              'tabs__label--selected': tab.id === selectedId,
             })}
-            onClick={() => onClick && onClick(tab.id)}
           >
-            <p
-              className={classNames('tabs__label', {
-                'tabs__label--selected': tab.id === selectedId,
-              })}
-            >
-              {tab.label}
-            </p>
-            {tab.quantity && <p className="tabs__qnty">{tab.quantity}</p>}
-          </div>
-        ))}
+            {tab.label}
+          </p>
+          {tab.quantity && <p className="tabs__qnty">{tab.quantity}</p>}
+        </div>
+      ))}
       <div className="tabs__tab-animated" style={animateStyle} />
     </div>
   );
